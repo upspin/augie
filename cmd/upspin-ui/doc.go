bc466b7620495000060e76c02dfd45a83f15635a
Andrew Gerrand <adg@golang.org>
2017-12-14 13:50:15 +1100

    cmd/upspin-ui: document the generated files and their locations
    Also rename the log file from browser.log to upspin-ui.log.
    
    Fix upspin/upspin#535
    
    Change-Id: Id3a8e0009bfd80b8094e71e29c35366883231293

--- cmd/upspin-ui/doc.go
+++ cmd/upspin-ui/doc.go
@@ -44,5 +44,23 @@ The "Refresh" button reloads the contents of the directory and displays it.
 
 The info buttons (a little "i" in a circle, to the right of each file) display
 extended information for a given directory entry.
+
+Files created by upspin-ui
+
+The signup process creates a config file at the location provided by the
+-config flag. The flag's default value is $HOME/upspin/config.
+Signup also generates key files and puts them in the directory
+$HOME/.ssh/$USER, where $USER is the Upspin user name.
+
+The upspinserver deployment process records its state in a file with the same
+name as the config file with the additional suffix ".gcpState".
+This state file is used to resume the deployment process should the upspin-ui
+program crash or be terminated by the user.
+Once deployment is complete this file may be removed.
+Deployment also generates key files which it puts in $HOME/.ssh/$USER,
+where $USER is the Upspin user name of the server being deployed.
+
+During the signup and deployment processes, upspin-ui logs debugging
+information to $HOME/upspin/log/upspin-ui.log.
 */
 package main
