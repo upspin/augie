27cf9b87e3722a69a2e8ab55bd874b86f31cd586
salman <s@aljmz.com>
2021-03-07 15:49:53 +0000

    augie: link to warden and add processes to menu
    This change rewrites the Augie macOS statusbar widget in Swift, and
    statically links it to upspin-warden. It displays warden's state
    using the cgo wrappers added in Iaedc51dd33e19b352c453e170c552ba3489196bb.
    
    Screenshot: https://sa.lj.am/misc/augie-screenshot.png
    
    Swift to Go via C is kinda gross, but lets us keep more code in
    familiar territory (Go). If we don't want to go that path I can
    probably figure out how to make HTTP requests, parse JSON, and
    monitor processes in Swift.
    
    To try it out:
    (0. add a replace directive to go.mod so exp.upspin.io points to Iaedc51dd33e19b352c453e170c552ba3489196bb)
    1. go build -tags carchive -buildmode=c-archive -o lib/libwarden.a exp.upspin.io/cmd/upspin-warden
    2. xcodebuild ONLY_ACTIVE_ARCH=NO -arch arm64  # probably x86_64 on amd64 but i haven't tried
    3. open ./build/Release/Augie.app
    
    Updates upspin/upspin#312
    
    Caveat: I wrote my first line of Swift this morning, and have never
    done any macOS GUI work before. I largely have no idea what I'm
    doing!
    
    This is something I wanted and seemed like a good exercise to learn
    linking Swift to Go, so I had a try.
    
    Change-Id: I1c13bea324ec7528531e60c1281092a38689e908

---
+++ Augie/Augie.swift
@@ -0,0 +1,149 @@
+// Copyright 2021 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import AppKit
+import SwiftUI
+import Warden
+
+struct LogView: View {
+	@State var log: String
+	var body: some View {
+		TextEditor(text: $log).font(.system(size: 14, design: .monospaced))
+	}
+}
+
+struct ProcMenuView: View {
+	@ObservedObject var proc: Proc
+	
+	var body: some View {
+		VStack(alignment: .leading, spacing: 4.0){
+			HStack{
+				Text(proc.name).fontWeight(.heavy)
+				Spacer()
+			}
+			HStack{
+				Text(proc.state())
+				Spacer()
+				Button("Log") {
+					proc.showLog()
+				}
+			}
+		}
+		.padding(.horizontal)
+	}
+}
+
+class Proc: ObservableObject {
+	let name: String
+	
+	var logWindow: NSWindow?
+	var menuItem: NSMenuItem?
+	
+	init(_ name: String) {
+		self.name = name
+		
+		menuItem = NSMenuItem()
+		menuItem?.view = NSHostingView(rootView: ProcMenuView(proc: self))
+		menuItem?.view?.frame = NSRect(x: 0, y: 0, width: 200, height: 50)
+		
+		logWindow = NSWindow(
+			contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+			styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+			backing: .buffered, defer: false)
+		logWindow?.isReleasedWhenClosed = false
+		logWindow?.center()
+		logWindow?.setFrameAutosaveName("Log: \(name)")
+		logWindow?.title = "Log: \(name)"
+	}
+	
+	/// state returns the current state of this process.
+	func state() -> String {
+		var cs = name.cString(using: .utf8)!
+		return withUnsafeMutablePointer(to: &cs[0]) { p -> String in
+			if let cstring = wardenProcState(p) {
+				defer {free(cstring)}
+				return String(cString: cstring)
+			}
+			return "<unknown>"
+		}
+	}
+	
+	/// log returns the tail of the logs for this process.
+	func log() -> String {
+		if var cs = name.cString(using: .utf8) {
+			return withUnsafeMutablePointer(to: &cs[0]) { p -> String in
+				if let cstring = wardenProcLog(p) {
+					defer {free(cstring)}
+					return String(cString: cstring)
+				}
+				return "<unknown>"
+			}
+		}
+		return "<unknown>"
+	}
+	
+	/// update invalidates any views subscribed to object and forces them to redraw.
+	func update() {
+		self.objectWillChange.send()
+	}
+	
+	func showLog() {
+		logWindow?.contentView = NSHostingView(rootView: LogView(log: log()))
+		logWindow?.makeKeyAndOrderFront(nil)
+		NSApp.activate(ignoringOtherApps: true)
+	}
+}
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+	var procs: [Proc] = []
+	var statusItem: NSStatusItem?
+
+	func initMenu() -> NSMenu {
+		let menu = NSMenu()
+		
+		// Add each process status item on the menu.
+		for p in procs {
+			if let item = p.menuItem {
+				menu.addItem(item)
+			}
+		}
+		
+		menu.addItem(NSMenuItem.separator())
+		
+		// Quit button.
+		let quitButton = NSMenuItem(title: "Quit Augie", action: #selector(NSApplication.shared.terminate), keyEquivalent: "")
+		menu.addItem(quitButton)
+		
+		return menu
+	}
+	
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Initialise the Go part and get list of configured commands.
+		if let cmds = wardenInit() {
+			procs = String(cString: cmds).split(separator: ",").map { Proc(String($0)) }
+			free(cmds)
+		}
+		
+		// Create a statusbar item and attach a menu to it.
+		self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+		self.statusItem?.menu = initMenu()
+		self.statusItem?.button?.title = "Augie"
+		let augieImage = NSImage(imageLiteralResourceName: "augie-menubar")
+		augieImage.isTemplate = true
+		self.statusItem?.button?.image = augieImage
+		
+		// Poll for changes in a process status.
+		// TODO register a callback in Go instead of polling.
+		Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { _ in
+			for p in self.procs {
+				p.update()
+			}
+		}
+	}
+	
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// TODO signal all child process and wait for a grace period.
+	}
+}
